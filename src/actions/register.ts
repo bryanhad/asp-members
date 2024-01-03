0a1aed96724c92520b2b3d9b6f18b7f54b6a838e
Bryan Hadinata <bryanhadinata76@gmail.com>
2024-01-03 17:44:47 +0700

    feat: register user now creates user to db

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -2,13 +2,38 @@
 
 import * as z from 'zod'
 import { RegisterSchema } from '@/schemas'
+import bcrypt from 'bcryptjs'
+import { db } from '@/lib/db'
+import { getUserByEmail } from '@/data/user'
 
-export const registerAction = async (values: z.infer<typeof RegisterSchema>) => {
+export const registerAction = async (
+    values: z.infer<typeof RegisterSchema>
+) => {
     const validatedFields = RegisterSchema.safeParse(values)
 
     if (!validatedFields.success) {
         return { error: 'Invalid fields' }
     }
 
-    return { success: 'Email sent' }
+    const { email, name, password } = validatedFields.data
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const emailTaken = await getUserByEmail(email)
+
+    if (emailTaken) {
+        return { error: 'Email already taken!' }
+    }
+
+    await db.user.create({
+        data: {
+            email,
+            name,
+            password: hashedPassword,
+        },
+    })
+
+    //TODO: Send verification email token
+
+    return { success: 'User created!' }
 }
