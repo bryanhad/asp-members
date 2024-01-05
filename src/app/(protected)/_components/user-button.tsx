c96825da2d3e07a2a988713b902bda9e22d5f977
Bryan Hadinata <bryanhadinata76@gmail.com>
2024-01-05 19:07:23 +0700

    fix: dropdown button now doesn't remove scrollbar

--- src/app/(protected)/_components/user-button.tsx
+++ src/app/(protected)/_components/user-button.tsx
@@ -13,18 +13,20 @@ import { AvatarImage } from '@radix-ui/react-avatar'
 import { FaUser } from 'react-icons/fa'
 import { LuChevronsLeftRight } from 'react-icons/lu'
 import { LogoutButton } from '@/components/auth/logout-button'
+import Link from 'next/link'
+import { IoSettingsOutline } from 'react-icons/io5'
 
 export const UserButtonn = () => {
     const user = useCurrentUser()
 
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
+        <DropdownMenu modal={false}>
+            <DropdownMenuTrigger asChild className="max-w-max">
                 <div
                     role="button"
                     className="flex items-center text-sm p-3 w-full hover:bg-primary/5"
                 >
-                    <div className="flex gap-2 items-center max-w-[150px]">
+                    <div className="flex gap-2 items-center">
                         <Avatar className="h-8 w-8">
                             <AvatarImage src={user?.profilePic || ''} />
                             <AvatarFallback className="bg-sky-500">
@@ -39,7 +41,7 @@ export const UserButtonn = () => {
                 </div>
             </DropdownMenuTrigger>
             <DropdownMenuContent
-                className="w-80"
+                className="w-[250px]"
                 align="end"
                 alignOffset={11}
                 forceMount
@@ -48,19 +50,33 @@ export const UserButtonn = () => {
                     <p className="text-xs font-medium leading-none text-muted-foreground">
                         {user?.email}
                     </p>
-                    <div className="flex items-center gap-2">
-                        <div className="rounded-md bg-secondary p-1">
-                            <Avatar className="h-8 w-8">
-                                <AvatarImage src={user?.profilePic || ''} />
-                                <AvatarFallback className="bg-sky-500">
-                                    <FaUser className="text-white" />
-                                </AvatarFallback>
-                            </Avatar>
-                        </div>
-                        <div className="space-y-1">
-                            <p className="text-sm line-clamp-1">{user?.name}</p>
-                        </div>
-                    </div>
+                    <DropdownMenuItem asChild className='cursor-pointer'>
+                        <Link
+                            href={'/settings'}
+                            className="py-1 px-2 hover:bg-secondary duration-300 rounded-sm"
+                        >
+                            <div className="flex items-center gap-2">
+                                <div className="rounded-md bg-secondary p-1">
+                                    <Avatar className="h-8 w-8">
+                                        <AvatarImage
+                                            src={user?.profilePic || ''}
+                                        />
+                                        <AvatarFallback className="bg-sky-500">
+                                            <FaUser className="text-white" />
+                                        </AvatarFallback>
+                                    </Avatar>
+                                </div>
+                                <div className="space-y-1 items-center flex gap-2">
+                                    <p className="text-sm line-clamp-1">
+                                        {user?.name}
+                                    </p>
+                                    <div>
+                                        <IoSettingsOutline className="opacity-50 mb-[2px]" />
+                                    </div>
+                                </div>
+                            </div>
+                        </Link>
+                    </DropdownMenuItem>
                 </div>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem asChild className="cursor-pointer">
