40f4ece5ac29abd8525cf79967e7a9905083ebc0
Bryan Hadinata <bryanhadinata76@gmail.com>
2024-01-12 15:56:44 +0700

    chore: add allowedOrigins

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,7 +15,7 @@ const { auth } = NextAuth(authConfig) // so isntead of getting the auth from the
 
 const allowedOrigins =
     process.env.NODE_ENV === 'production'
-        ? ['https://mydomain.com', 'https://www.mydomain.com']
+        ? ['https://asp-members.vercel.app', 'https://www.mydomain.com', 'http://localhost:3000', 'http://localhost:3001']
         : ['http://localhost:3000', 'http://localhost:3001']
 
 export default auth(async (req) => {
