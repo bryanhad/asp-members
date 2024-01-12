c58328401ecaa7ee4748db96bd05b4e83f6a26e6
Bryan Hadinata <bryanhadinata76@gmail.com>
2024-01-12 09:31:17 +0700

    fix: delete button on users table now is only shown to admin

--- src/app/(protected)/users/_components/columns.tsx
+++ src/app/(protected)/users/_components/columns.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { deleteUser } from '@/actions/admin'
+import { OnlyShowToAdmin } from '@/components/auth/only-show-to-admin'
 import DeleteButtonModal from '@/components/delete-button-modal'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
@@ -62,7 +63,7 @@ export const columns: ColumnDef<FetchedUser>[] = [
         accessorKey: 'createdAt',
         header: () => <div className="text-right">Joined Since</div>,
         cell: ({ row }) => {
-            const date = row.getValue('createdAt') as Date 
+            const date = row.getValue('createdAt') as Date
 
             return (
                 <div className="text-right font-medium">
@@ -101,23 +102,26 @@ export const columns: ColumnDef<FetchedUser>[] = [
                                 <p>View</p>
                             </Link>
                         </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>
+                        {/* <DropdownMenuItem>
                             <Link
                                 href={`/users/${user.id}/edit`}
                                 className="w-full"
                             >
                                 <p>Edit</p>
                             </Link>
-                        </DropdownMenuItem>
-                        <DeleteButtonModal
-                            onConfirm={() => deleteUser(user.id)}
-                            description={`User '${user.name}' will be deleted permanently.`}
-                        >
-                            <p className="w-full text-destructive font-semibold cursor-pointer p-1 rounded-md hover:bg-secondary duration-200">
-                                delete
-                            </p>
-                        </DeleteButtonModal>
+                        </DropdownMenuItem> */}
+                        <OnlyShowToAdmin ownerId={user.id}>
+                            <DropdownMenuSeparator />
+
+                            <DeleteButtonModal
+                                onConfirm={() => deleteUser(user.id)}
+                                description={`User '${user.name}' will be deleted permanently.`}
+                            >
+                                <p className="w-full text-destructive font-semibold cursor-pointer p-1 rounded-md hover:bg-secondary duration-200">
+                                    delete
+                                </p>
+                            </DeleteButtonModal>
+                        </OnlyShowToAdmin>
                     </DropdownMenuContent>
                 </DropdownMenu>
             )
